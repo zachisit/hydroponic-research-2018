f7a1fe778831c6fad9795e2f0d3b30d2ecd6ca11
Zachary Smith <info@zachis.it>
2018-01-29 17:06:28 -0500

    now shop_add_var_to_cart allows for variation or non-variation product to be added to cart via ajax; updated product_by_cat.template to accomodate

--- js/shop_add_var_to_cart.js
+++ js/shop_add_var_to_cart.js
@@ -2,28 +2,83 @@
  * File shop_add_var_to_cart.js
  *
  * allow variations to be added to cart outside
- * woocommerce single product view
+ * WooCommerce single product view
  *
- * @depenency jquery
+ * @depenency jquery, ajax, javascript
  * @version 1.0.0
  * @package hydroponic-research
  *
  */
 jQuery(document).ready(function($) {
-    //the button link that shows dynamically under the text
-    var $addToCartButton = $('#shop_add_var_to_cart');
+    var $addToCartButton = $('.shop_add_var_to_cart');
 
     $addToCartButton.click(function( ) {
-        $.ajax( {
-            url: '?add-to-cart=70&variation_id=114',
-            data:'message',
-            type:'POST',
-            success: function(resp) {
-                window.alert('success')
-            },
-            error: function(e) {
-                window.alert(e);
-            }
-        })
-    })
+        var $productVariationNumber = $(this).closest('div').find('.weight_dropdown').find('#weight').val(),
+            $productName = $(this).closest('div').find('.product_title').html(),
+            $productID = $(this).closest('li').attr('data-id');
+
+        if ($productVariationNumber) {
+            console.log('product name: '+$productName+' and prouct id: '+$productID+' and product variation number: '+$productVariationNumber);
+            productPost(1, $productID, $productVariationNumber, $productName);
+        } else { //no weight variation
+            productPost(0, $productID, $productVariationNumber, $productName);
+        }
+    });
+    
+    /**
+     * Add Product to Cart
+     *
+     * Utilizes WooCommerce add_to_cart via url method to
+     * post via AJAX into cart
+     *
+     * @param hasVariation
+     * @param productID
+     * @param productVariationID
+     * @param productName
+     */
+    function productPost(hasVariation, productID, productVariationID, productName) {
+        var $dialog = document.getElementById('orderAdded');//@TODO;move this into dynamic created object
+
+        if (hasVariation === 1) {//if has a variation
+            $.ajax( {
+                url: '?add-to-cart='+productID+'&variation_id='+productVariationID+'&quantity=1',
+                data:'message',//@TODO:correct usage?
+                type:'POST',
+                success: function(resp) {
+                    $dialog.innerHTML = '<h2>Success!</h2><p>Yay. The Product <strong>'+productName+'</strong> is added to your cart</p>';
+                    $dialog.showModal();
+                    setTimeout(function() {
+                        $dialog.close();
+                    }, 2900);
+                },
+                error: function(e) {
+                    $dialog.innerHTML = '<h2>Error</h2><p>Error 1111: '+e+'</p>';
+                    $dialog.showModal();
+                    setTimeout(function() {
+                        $dialog.close();
+                    }, 2900);
+                }
+            })
+        } else {
+            $.ajax( {
+                url: '?add-to-cart='+productID+'&quantity=1',
+                data:'message',//@TODO:correct usage?
+                type:'POST',
+                success: function(resp) {
+                    $dialog.innerHTML = '<h2>Success!</h2><p>Yay. The Product <strong>'+productName+'</strong> is added to your cart</p>';
+                    $dialog.showModal();
+                    setTimeout(function() {
+                        $dialog.close();
+                    }, 2900);
+                },
+                error: function(e) {
+                    $dialog.innerHTML = '<h2>Error</h2><p>Error 1111: '+e+'</p>';
+                    $dialog.showModal();
+                    setTimeout(function() {
+                        $dialog.close();
+                    }, 2900);
+                }
+            })
+        }
+    }
 });
--- templates/shortcode/product_by_cat.template.php
+++ templates/shortcode/product_by_cat.template.php
@@ -11,38 +11,51 @@ if(isset($posts) && !empty($posts)): ?>
     <ul class="product_category_landing_list_style">
         <?php foreach($posts as $post):
             $product = wc_get_product($post->ID);
-            //$variation_data = $product->get_variation_attributes();
-            ?>
-            <li>
+            $productID = $product->get_id();
+            $productTitle = $post->post_title; ?>
+            <li data-id="<?=$productID?>">
                 <div class="product_item">
-                    <a href="<?=get_post_permalink($post) ?>" title="<?=$post->post_title?>">
+                    <a href="<?=get_post_permalink($post) ?>" title="<?=$productTitle?>">
                         <div class="thumbnail"><?=get_the_post_thumbnail($post, 'medium');?></div>
-                        <h2 class="product_title"><?=$post->post_title?></h2>
+                        <h2 class="product_title"><?=$productTitle?></h2>
                     </a>
                     <span class="product_cost"><?=$product->get_price_html()?></span>
-                    <div class="weight_dropdown">
+                    <?php
+                    //product's total variation data
+                    $productChildren = $product->get_children();
+
+                    if ($productChildren) :
+                        //product's variation IDs
+                        $productVariations = $product->get_variation_attributes();
+
+                        $productChildrenArray = [];
+                        //variation id number
+                        foreach ($productChildren as $value) {
+                            array_push($productChildrenArray, $value);
+                        }
+
+                        //variation name
+                        $productVariationsArray = [];
+                        foreach ($productVariations as $i => $values) {
+                            foreach ($values as $key => $value) {
+                                array_push($productVariationsArray, $value);
+
+                            }
+                        }
+
+                        //merge both arrays into one with key=>value we can use for option select
+                        $list = array_combine($productChildrenArray, $productVariationsArray); ?>
+                        <div class="weight_dropdown">
                         <label for="weight">Weight</label>
                         <select id="weight" name="weight">
-                            <option>0.9 oz</option>
-                            <option>1.5 oz</option>
-                            <option>2.8 oz</option>
-                            <option>3.5 oz</option>
-                            <option>5 oz</option>
+                            <option value="Select Your Weight">Select Your Weight</option>
+                            <?php foreach ($list as $key=>$value) : ?>
+                            <option value="<?=$key?>" data-name="<?=$productTitle?>" data-id="<?=$key?>"><?=$value?></option>
+                            <?php endforeach ?>
                         </select>
-                    </div>
-<!--                    --><?//=do_shortcode('[add_to_cart id="70"]')?>
-                    <?php
-                    #111 is the id
-                    //var_dump($variation_data);
-//                    echo '<select>';
-//                    foreach ($variation_data as $key) {
-//                        foreach ($key as $value) {
-//                            echo '<option>'.$value.'</option>';
-//                        }
-//                    }
-//                    echo '</select>';
-                    ?>
-<button id="shop_add_var_to_cart">click</button>
+                        </div>
+                    <?php endif ?>
+                    <button class="shop_add_var_to_cart">add to cart</button>
                 </div>
             </li>
         <?php endforeach;?>
@@ -55,3 +68,4 @@ if(isset($posts) && !empty($posts)): ?>
 <?php else:
 echo '<p>No products found in this category</p>';
 endif;?>
+<dialog class="generalDialog" id="orderAdded"><!-- --></dialog>
