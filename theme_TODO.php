379639e22f0131769c3c63a5c3249d82af1ba8da
Zachary Smith <info@zachis.it>
2018-04-05 09:00:02 -0400

    updated todo file per product edit

--- theme_TODO.php
+++ theme_TODO.php
@@ -9,3 +9,10 @@
 //@TODO:create 'show related articles, etc' module block for pages
 //@TODO:create top banner to show if something in cart
 //@TODO;remove any errant console logs from js files
+//@TODO:add htaccess /my-account to go to /grow-files
+//@TODO:product cat landing page query: change $post to $product so clear; refactor frontend logic if needed
+//@TODO:change "left" sidebar on user profile to be "sidebar"
+//@TODO:add tabindex to user profile edit; any other pages?
+//@TODO:user profile edit: sidebar "if element exists show data" not working as is not pulling in array value from edit template page
+//@TODO:placeholder/fallback image for product inputs
+//@TODO:cleanup product backend field UX
